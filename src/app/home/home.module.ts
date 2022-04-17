79fe54f9bae3c3060dae20fa34aeef08060d99e1
Firhan Hardiansyah <firhan.hardiansyah09@gmail.com>
2022-04-17 11:31:57 +0700

    Add Home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { NoteDetailPageModule } from '../modals/note-detail/note-detail.module';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NoteDetailPageModule
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AlertController, ModalController } from '@ionic/angular';
+import { DataService } from '../@core/services/data.service';
+import { NoteDetailPage } from '../modals/note-detail/note-detail.page';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,66 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  loading?: boolean = true;
+  notes = [];
 
+  constructor(
+    private dataService: DataService,
+    private alertController: AlertController,
+    private modalController: ModalController
+  ) {
+    this.getNotes();
+  }
+
+  getNotes() {
+    this.dataService.getNotes().subscribe((res) => {
+      this.loading = false;
+      this.notes = res;
+    });
+  }
+
+  async openNote(note) {
+    const modal = await this.modalController.create({
+      component: NoteDetailPage,
+      componentProps: { id: note.id },
+      breakpoints: [0, 0.5, 0.8],
+      initialBreakpoint: 0.5,
+    });
+    modal.present();
+  }
+
+  async addNote() {
+    const alert = this.alertController.create({
+      header: 'Add Note',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title cuy',
+          type: 'text',
+        },
+        {
+          name: 'description',
+          placeholder: 'Lorem ipsum dolor sit amet cuy',
+          type: 'textarea',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Add',
+          handler: (res) => {
+            this.dataService.addNote({
+              title: res.title,
+              description: res.description
+            });
+          },
+        },
+      ],
+    });
+
+    (await alert).present();
+  }
 }
